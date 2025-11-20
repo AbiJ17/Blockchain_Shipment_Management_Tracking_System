e32e34a3641e94344fa233a16deafde3c89a8c77
AbiJ17 <abinashjegan17@gmail.com>
2025-11-19 22:22:23 -0500

    implemented classes in external package

--- Blockchain_Shipment_Management_Tracking_System_cps731/src/external/BlockchainNetwork.java
+++ Blockchain_Shipment_Management_Tracking_System_cps731/src/external/BlockchainNetwork.java
@@ -1,5 +1,26 @@
 package external;
 
+import java.util.List;
+
+import org.w3c.dom.Node;
+
 public class BlockchainNetwork {
+
+    public List<Node> nodeList; 
+    public String consensusAlgorithm;
+   
+    public List<Node> getNodeList() {
+        return nodeList;
+    }
+    public void setNodeList(List<Node> nodeList) {
+        this.nodeList = nodeList;
+    }
+    public String getConsensusAlgorithm() {
+        return consensusAlgorithm;
+    }
+    public void setConsensusAlgorithm(String consensusAlgorithm) {
+        this.consensusAlgorithm = consensusAlgorithm;
+    } 
+
     
 }
--- Blockchain_Shipment_Management_Tracking_System_cps731/src/external/OffChainStorage.java
+++ Blockchain_Shipment_Management_Tracking_System_cps731/src/external/OffChainStorage.java
@@ -1,5 +1,29 @@
 package external;
 
+import java.util.List;
+
+// import javax.swing.text.Document;
+
 public class OffChainStorage {
+
+    public boolean avaliable;
+    // public List <Document> documents; 
+
+    public boolean isAvaliable() {
+        return avaliable;
+    }
+
+    public void setAvaliable(boolean avaliable) {
+        this.avaliable = avaliable;
+    }
+
+    // public List<Document> getDocuments() {
+    //     return documents;
+    // }
+
+    // public void setDocuments(List<Document> documents) {
+    //     this.documents = documents;
+    // }
+    
     
 }
